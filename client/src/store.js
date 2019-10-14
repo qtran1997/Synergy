985aedaf2ee066704eb436cd4741aa29d0a2dc95
Quang Tran <qtran1997@gmail.com>
2019-10-13 22:41:04 -0700

    Added dependencies: action logger, scss loader, internationalization(react-intl)

--- client/src/store.js
+++ client/src/store.js
@@ -1,26 +1,33 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import { createLogger } from "redux-logger";
 
 const initialState = {};
 
-const middleware = [thunk];
+const logger = createLogger({
+  duration: true,
+  timestape: true,
+  collapsed: true
+});
+
+const middleware = [thunk, logger];
 
 let store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
-if (process.env.NODE_ENV === "production") {
-  store = createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(...middleware)
-  );
-}
+// if (process.env.NODE_ENV === "production") {
+//   store = createStore(
+//     rootReducer,
+//     initialState,
+//     applyMiddleware(...middleware)
+//   );
+// }
 
 export default store;
