dfbe37425f39844c3a331d58972321ab158382f6
Quang Tran <qtran1997@gmail.com>
2019-10-20 01:21:29 -0700

    created component directory indexes

---
+++ client/src/components/index.js
@@ -0,0 +1,14 @@
+export { default as Dock } from "./layout/Dock/Dock";
+export { default as Icon } from "./layout/Icon/Icon";
+export { default as Landing } from "./layout/Landing/Landing";
+export { default as Login } from "./auth/Login/Login";
+export { default as MainApp } from "./layout/Main/Main";
+export { default as MainMenu } from "./layout/Menu/MainMenu";
+export { default as Note } from "./Note/Note";
+export { default as Notepad } from "./Notepad/Notepad";
+export { default as NotFound } from "./layout/NotFound/NotFound";
+export { default as Register } from "./auth/Register/Register";
+export { default as Settings } from "./layout/Settings/Settings";
+export {
+  default as TextFieldGroup
+} from "./common/TextFieldGroup/TextFieldGroup";
---
+++ client/src/components/layout/index.js
@@ -0,0 +1,7 @@
+export { default as Dock } from "./Dock/Dock";
+export { default as Icon } from "./Icon/Icon";
+export { default as Landing } from "./Landing/Landing";
+export { default as MainApp } from "./Main/Main";
+export { default as MainMenu } from "./Menu/MainMenu";
+export { default as NotFound } from "./NotFound/NotFound";
+export { default as Settings } from "./Settings/Settings";
