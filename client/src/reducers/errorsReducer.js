5c2ad011702b4b087dcaca6992cf14e43f392dfd
Quang Tran <qtran1997@gmail.com>
2019-12-01 21:11:54 -0800

    renamed errorReducer to errorsReducer

--- client/src/reducers/errorsReducer.js
+++ client/src/reducers/errorsReducer.js
@@ -4,11 +4,6 @@ const initialState = {};
 
 export default function(state = initialState, action) {
   switch (action.type) {
-    case GET_SUCCESSES:
-      return {
-        successes: action.payload.successes,
-        errors: action.payload.errors
-      };
     case GET_ERRORS:
       return action.payload;
     default:
