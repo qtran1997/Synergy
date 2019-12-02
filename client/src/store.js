111e0251224e72d7c3c1107c8f54b21c6bd4a68a
Quang Tran <qtran1997@gmail.com>
2019-12-01 21:10:01 -0800

    uncommented conditional development and production store creation

--- client/src/store.js
+++ client/src/store.js
@@ -17,17 +17,17 @@ let store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware)
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
-// if (process.env.NODE_ENV === "production") {
-//   store = createStore(
-//     rootReducer,
-//     initialState,
-//     applyMiddleware(...middleware)
-//   );
-// }
+if (process.env.NODE_ENV === "production") {
+  store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(...middleware)
+  );
+}
 
 export default store;
