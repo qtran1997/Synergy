f8679750abe094ec6f13cf0197d923472219a216
Quang Tran <qtran1997@gmail.com>
2019-12-04 22:10:53 -0800

    Created and initalized Board component

---
+++ client/src/components/layout/Board/Board.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from "react";
+
+import "./Board.scss";
+
+class Board extends PureComponent {
+  render() {
+    return <div className='board-container'>BOARD CONTENT</div>;
+  }
+}
+
+export default Board;
