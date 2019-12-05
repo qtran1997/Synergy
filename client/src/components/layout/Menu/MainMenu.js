6716d81e0b36ee20493ebc96e74a16f601684134
Quang Tran <qtran1997@gmail.com>
2019-12-04 23:06:34 -0800

    rewrote the entire main menu component to be a dock instead of a popup

--- client/src/components/layout/Menu/MainMenu.js
+++ client/src/components/layout/Menu/MainMenu.js
@@ -1,84 +1,41 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { withRouter } from "react-router-dom";
+import React, { PureComponent } from "react";
+import { Form, FormControl } from "react-bootstrap";
 
-import Button from "@material-ui/core/Button";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
+import SimpleBar from "simplebar-react";
+import "simplebar/dist/simplebar.min.css";
 
-import { Icon } from "../";
-import { logoutUser } from "../../../actions/authActions";
+import { Icon } from "../index";
 
 import "./MainMenu.scss";
 
-/**
- * The menu button found inside of the Dock component that handles user settings and authentication
- */
-const MainMenu = ({ history, logoutUser }) => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+class MainMenu extends PureComponent {
+  render() {
+    return (
+      <SimpleBar className='main-menu-box'>
+        <div className='main-menu-box-header'>
+          <button
+            className='main-menu-box-icon-button'
+            onClick={() => console.log("FLKDJSLFKSDJK")}
+          >
+            <Icon name='Search' />
+          </button>
+          <Form inline>
+            <FormControl
+              type='text'
+              placeholder='Search'
+              className='main-menu-box-search-bar'
+            />
+          </Form>
+        </div>
+        <div className='main-menu-box-online'>
+          <p>Online - </p>
+        </div>
+        <div className='main-menu-box-offline'>
+          <p>Offline - </p>
+        </div>
+      </SimpleBar>
+    );
+  }
+}
 
-  const handleClick = event => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const onLogoutClick = e => {
-    logoutUser();
-    history.push("/");
-  };
-
-  const menuItems = (
-    <div className='main-menu'>
-      <MenuItem className='main-menu-item' onClick={handleClose}>
-        <Link className='main-menu-link' to='/profile'>
-          Profile
-        </Link>
-      </MenuItem>
-      <MenuItem className='main-menu-item' onClick={handleClose}>
-        <Link className='main-menu-link' to='/settings'>
-          My Account
-        </Link>
-      </MenuItem>
-      <MenuItem
-        className='main-menu-item'
-        onClick={() => {
-          onLogoutClick();
-          handleClose();
-        }}
-      >
-        Logout
-      </MenuItem>
-    </div>
-  );
-
-  return (
-    <div>
-      <Button
-        aria-controls='simple-menu'
-        aria-haspopup='true'
-        onClick={handleClick}
-      >
-        <Icon name='Dehaze' />
-      </Button>
-      <Menu
-        id='simple-menu'
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        {menuItems}
-      </Menu>
-    </div>
-  );
-};
-
-const mapDispatchToProps = {
-  logoutUser
-};
-
-export default connect(null, mapDispatchToProps)(withRouter(MainMenu));
+export default MainMenu;
