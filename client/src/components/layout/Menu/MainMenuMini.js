59d0d9b7637e1fd1909b830dc0fefd66abb1bc6b
Quang Tran <qtran1997@gmail.com>
2019-12-07 15:07:13 -0800

    added Scrollable functionality to buttons in MainMenuMini component

--- client/src/components/layout/Menu/MainMenuMini.js
+++ client/src/components/layout/Menu/MainMenuMini.js
@@ -1,12 +1,124 @@
 import React, { PureComponent } from "react";
 
-// import { Icon } from "../index";
+import Fab from "@material-ui/core/Fab";
+
+import SimpleBar from "simplebar-react";
+import "simplebar/dist/simplebar.min.css";
 
 import "./MainMenu.scss";
 
 class MainMenuMini extends PureComponent {
   render() {
-    return <div className='main-menu-mini'></div>;
+    return (
+      <div className='main-menu-mini'>
+        <SimpleBar className='main-menu-mini-scrollable'>
+          <div className='main-menu-mini-icon-container'>
+            <Fab
+              color='primary'
+              aria-label='add'
+              className='main-menu-mini-add-button'
+              onClick={() => {}}
+            ></Fab>
+          </div>
+          <div className='main-menu-mini-icon-container'>
+            <Fab
+              color='primary'
+              aria-label='add'
+              className='main-menu-mini-add-button'
+              onClick={() => {}}
+            ></Fab>
+          </div>
+          <div className='main-menu-mini-icon-container'>
+            <Fab
+              color='primary'
+              aria-label='add'
+              className='main-menu-mini-add-button'
+              onClick={() => {}}
+            ></Fab>
+          </div>
+          <div className='main-menu-mini-icon-container'>
+            <Fab
+              color='primary'
+              aria-label='add'
+              className='main-menu-mini-add-button'
+              onClick={() => {}}
+            ></Fab>
+          </div>
+          <div className='main-menu-mini-icon-container'>
+            <Fab
+              color='primary'
+              aria-label='add'
+              className='main-menu-mini-add-button'
+              onClick={() => {}}
+            ></Fab>
+          </div>
+          <div className='main-menu-mini-icon-container'>
+            <Fab
+              color='primary'
+              aria-label='add'
+              className='main-menu-mini-add-button'
+              onClick={() => {}}
+            ></Fab>
+          </div>
+          <div className='main-menu-mini-icon-container'>
+            <Fab
+              color='primary'
+              aria-label='add'
+              className='main-menu-mini-add-button'
+              onClick={() => {}}
+            ></Fab>
+          </div>
+          <div className='main-menu-mini-icon-container'>
+            <Fab
+              color='primary'
+              aria-label='add'
+              className='main-menu-mini-add-button'
+              onClick={() => {}}
+            ></Fab>
+          </div>
+          <div className='main-menu-mini-icon-container'>
+            <Fab
+              color='primary'
+              aria-label='add'
+              className='main-menu-mini-add-button'
+              onClick={() => {}}
+            ></Fab>
+          </div>
+          <div className='main-menu-mini-icon-container'>
+            <Fab
+              color='primary'
+              aria-label='add'
+              className='main-menu-mini-add-button'
+              onClick={() => {}}
+            ></Fab>
+          </div>
+          <div className='main-menu-mini-icon-container'>
+            <Fab
+              color='primary'
+              aria-label='add'
+              className='main-menu-mini-add-button'
+              onClick={() => {}}
+            ></Fab>
+          </div>
+          <div className='main-menu-mini-icon-container'>
+            <Fab
+              color='primary'
+              aria-label='add'
+              className='main-menu-mini-add-button'
+              onClick={() => {}}
+            ></Fab>
+          </div>
+          <div className='main-menu-mini-icon-container'>
+            <Fab
+              color='primary'
+              aria-label='add'
+              className='main-menu-mini-add-button'
+              onClick={() => {}}
+            ></Fab>
+          </div>
+        </SimpleBar>
+      </div>
+    );
   }
 }
 
