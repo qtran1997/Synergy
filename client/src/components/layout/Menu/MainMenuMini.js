15546ebb5fba558448720fa02fc2d5dc79a79064
Quang Tran <qtran1997@gmail.com>
2019-12-16 23:17:57 -0800

    Added Create button for Notepad in mini main menu

--- client/src/components/layout/Menu/MainMenuMini.js
+++ client/src/components/layout/Menu/MainMenuMini.js
@@ -2,34 +2,165 @@ import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
+import Fab from "@material-ui/core/Fab";
+import Modal from "@material-ui/core/Modal";
+
 import SimpleBar from "simplebar-react";
 import "simplebar/dist/simplebar.min.css";
 
 import NotepadIcon from "./NotepadIcon/NotepadIcon";
 
+import { Fade, Icon, TextFieldGroup } from "../../";
+
+import { changeNotepad } from "../../../actions/layoutActions";
+import { createNotepad, getNotes } from "../../../actions/notepadActions";
+
 import "./MainMenu.scss";
 
 class MainMenuMini extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      createNotepadModal: false,
+      errors: {},
+      notepadTitle: "",
+      notepadDescription: ""
+    };
+
+    this.newNotepadOnChange = this.newNotepadOnChange.bind(this);
+    this.newNotepadOnSubmit = this.newNotepadOnSubmit.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    // Created new notepad and want modal to disappear
+    if (
+      Object.keys(prevProps.notepads).length <
+      Object.keys(this.props.notepads).length
+    ) {
+      this.setState({ createNotepadModal: false });
+    }
+
+    // Notepad has been changed
+    if (prevProps.currentNotepadId !== this.props.currentNotepadId) {
+      this.props.getNotes(
+        this.props.currentNotepadId,
+        this.props.notepads[this.props.currentNotepadId]
+      );
+    }
+  }
+
+  newNotepadOnChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  newNotepadOnSubmit(e) {
+    e.preventDefault();
+
+    const notepadData = {
+      title: this.state.notepadTitle,
+      description: this.state.notepadDescription
+    };
+
+    this.props.createNotepad(notepadData);
+  }
+
+  openNotepadModal() {
+    this.setState({
+      createNotepadModal: true
+    });
+  }
+
+  closeNotepadModal() {
+    this.setState({
+      createNotepadModal: false
+    });
+  }
+
   render() {
     const notepads = Object.values(this.props.notepads).map(({ id, title }) => (
-      <NotepadIcon key={id} title={title} />
+      <NotepadIcon
+        key={id}
+        title={title}
+        id={id}
+        action={() => {
+          this.props.changeNotepad(id);
+        }}
+      />
     ));
     return (
       <div className='main-menu-mini'>
         <SimpleBar className='main-menu-mini-scrollable'>
           {[...notepads]}
+          <NotepadIcon
+            key='create-notepad'
+            title='Create New Notepad'
+            picture={<Icon name='Add' />}
+            id='create-notepad'
+            action={() => this.openNotepadModal()}
+          />
         </SimpleBar>
+        <Modal
+          aria-labelledby='notepad-create-modal-title'
+          aria-describedby='notepad-create-modal-description'
+          className='notepad-create-modal'
+          open={this.state.createNotepadModal}
+          onClose={() => this.closeNotepadModal()}
+        >
+          <Fade in={this.state.createNotepadModal}>
+            <div className='notepad-create-modal-container'>
+              <h2 id='notepad-create-modal-title'>Create New Notepad</h2>
+              <hr />
+              <TextFieldGroup
+                placeholder='Notepad Name'
+                name='notepadTitle'
+                type='text'
+                label='Notepad Name'
+                value={this.state.notepadTitle}
+                onChange={this.newNotepadOnChange}
+                error={this.state.errors.notepadTitle}
+              />
+              <TextFieldGroup
+                placeholder='Notepad Description'
+                name='notepadDescription'
+                type='text'
+                label='Notepad Description'
+                value={this.state.notepadDescription}
+                onChange={this.newNotepadOnChange}
+                error={this.state.errors.notepadDescription}
+              />
+              <div className='notepad-create-modal-button'>
+                <Fab
+                  variant='extended'
+                  color='primary'
+                  aria-label='add'
+                  onClick={() => this.props.createNotepad()}
+                >
+                  CREATE
+                </Fab>
+              </div>
+            </div>
+          </Fade>
+        </Modal>
       </div>
     );
   }
 }
 
 MainMenuMini.propTypes = {
+  currentNotepadId: PropTypes.string.isRequired,
   notepads: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
+  currentNotepadId: state.layout.main.display.notepad,
   notepads: state.layout.main.notepads
 });
 
-export default connect(mapStateToProps, null)(MainMenuMini);
+const mapDispatchToProps = {
+  changeNotepad,
+  createNotepad,
+  getNotes
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainMenuMini);
--- client/src/components/layout/Menu/NotepadIcon/NotepadIcon.js
+++ client/src/components/layout/Menu/NotepadIcon/NotepadIcon.js
@@ -1,25 +1,29 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 import Fab from "@material-ui/core/Fab";
 import Tooltip from "@material-ui/core/Tooltip";
+import Zoom from "@material-ui/core/Zoom";
 
 import "./NotepadIcon.scss";
 
 class NotepadIcon extends PureComponent {
   render() {
-    const { picture, title } = this.props;
+    const { action, picture, title } = this.props;
 
     // Display picture or display the first letter of the notepad if the picture does not exist
     const icon = picture ? picture : title.substring(0, 1);
 
     return (
       <div className='notepad-icon-button-container'>
-        <Tooltip title={title} placement='right'>
+        <Tooltip TransitionComponent={Zoom} title={title} placement='right'>
           <Fab
             color='primary'
             className='notepad-icon-button'
-            onClick={() => {}}
+            onClick={() => {
+              action();
+            }}
           >
             {icon}
           </Fab>
@@ -30,8 +34,13 @@ class NotepadIcon extends PureComponent {
 }
 
 NotepadIcon.propTypes = {
-  picture: PropTypes.string,
-  title: PropTypes.string.isRequired
+  picture: PropTypes.any, // TODO: CHANGE THIS
+  title: PropTypes.string.isRequired,
+  action: PropTypes.func.isRequired
 };
 
-export default NotepadIcon;
+const mapStateToProps = state => ({
+  notepads: state.layout.main.notepads
+});
+
+export default connect(mapStateToProps, null)(NotepadIcon);
